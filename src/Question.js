92fa16d8e2843fb68472415981bcd652c90825c1
Nikoloz Tsutskiridze <nikacucqiridze2000@gmail.com>
2024-08-23 20:27:40 +0400

    First-Question

---
+++ src/Question.js
@@ -0,0 +1,17 @@
+function Question({ question }) {
+  return (
+    <div>
+      <h4>{question.question}</h4>
+
+      <div className="options">
+        {question.options.map((options) => (
+          <button className="btn btn-option" key={options}>
+            {options}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Question;
